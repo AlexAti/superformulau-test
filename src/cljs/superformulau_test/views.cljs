66c8e41b8228a44febce6f189c93f9873c8172c7
Alex Ati <alejandro@atienzaramos.com>
2016-08-05 18:45:27 +0200

    Added a superformula D3 graph

--- src/cljs/superformulau_test/views.cljs
+++ src/cljs/superformulau_test/views.cljs
@@ -1,5 +1,7 @@
 (ns superformulau-test.views
-    (:require [re-frame.core :as re-frame]))
+    (:require [re-frame.core :as re-frame]
+              [reagent.core :as reagent]
+              [cljsjs.d3]))
 
 (defn slider [name [min max] val]
   [:div
@@ -25,8 +27,81 @@
                 [-100 100]
                 (k @sfu-values)]))])))
 
+(defn superformulau-radium [angle [a b y z n1 n2 n3]]
+  (Math.pow
+    (+ (Math.pow (Math.abs (/ (Math.cos (* y angle (/ 1 4))) a)) n2)
+       (Math.pow (Math.abs (/ (Math.sin (* z angle (/ 1 4))) b)) n3))
+    (- (/ 1 n1))))
+
+(defn polar-to-cartesian [[angle radium]]
+  "(polar-to-cartesian point) returns a [x y] point from an [a r] one."
+  [(* radium (Math.cos angle))
+   (* radium (Math.sin angle))])
+
+(defn sfu-figure
+  "Returns a superformulaU based figure. Parameters for the function are:
+   - [Mandatory] size: The radium of the base circle.
+   - [Optional] position: An [x y] vector. (0,0) if not provided.
+   - [Optional] resolution: The number of defining points. 64 if not provided.
+   - [Optional] params: A vector of 7 params that define the figure. Random if not provided."
+  ([size]
+   (sfu-figure size [0 0]))
+  ([size position]
+   (sfu-figure size position 64))
+  ([size position resolution]
+   (sfu-figure size position resolution (take 7 (repeatedly rand))))
+  ([size [x y] resolution params]
+   (let [pi2 (* 2 Math.PI)
+         steps (range 0 pi2 (/ pi2 resolution))
+         radiums (map #(* size (superformulau-radium % params)) steps)
+         polarcoords (partition 2 (interleave steps radiums))]
+    ;      coords (map polar-to-cartesian polarcoords)]
+    ;  (map (fn [[i j]] [(+ x i)
+    ;                    (+ y j)])
+    ;       coords)
+    ;; NOTE: Trnsalation doesnt work for polarcoords in radialLine
+    ;; (see https://github.com/d3/d3-shape#lines)
+     polarcoords)))
+
+(defn sfu-paint [values position]
+  (let [valuevec (map values [:a :b :y :z :n1 :n2 :n3]) ; to ensure proper param order
+        d3values (clj->js [(sfu-figure 30 position 128 valuevec)])]
+    ; Enter method
+    (.. js/d3
+        (select "svg.sfu")
+        (selectAll "path")
+        (data d3values)
+        enter
+        (append "svg:path")
+        (attr "d" (.radialLine js/d3)))
+
+    ; Update method
+    (.. js/d3
+        (select "svg.sfu")
+        (selectAll "path")
+        (data d3values)
+        (attr "d" (.radialLine js/d3)))
+
+    ; Exit method
+    (.. js/d3
+        (select "svg.sfu")
+        (selectAll "path")
+        (data d3values)
+        exit
+        (remove))))
+
+(defn sfu-comp [values position]
+  (reagent/create-class
+    {:display-name "SuperformulaU component"
+     :reagent-render (fn [] [:svg.sfu])
+     :component-did-mount #(sfu-paint values position)
+     :component-did-update #(let [[_ values] (reagent/argv %)] (sfu-paint values position))}))
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
+  (let [name (re-frame/subscribe [:name])
+        sfu-values (re-frame/subscribe [:sfu-values])]
     (fn []
-      [:div "Hello from " @name]
-      [slider-panel])))
+      [:div
+        [:div "Hello from " @name]
+        [slider-panel]
+        [sfu-comp @sfu-values [100 100]]])))
